751f1c4e13e3da28df927f4d993e5fb0c885edcb
Thalles Leite <eng.thallesleite@gmail.com>
2023-02-23 15:23:19 -0300

    fix: ajuste do css.

--- src/pages/cepv2.js
+++ src/pages/cepv2.js
@@ -1,1 +1,9 @@
-console.log('teste');
+const apiCep = document.getElementById('apiCep');
+const sectionApi = document.getElementById('api');
+
+apiCep.addEventListener('click', () => {
+  const cepContainer = document.createElement('section');
+  cepContainer.className('cepContainer');
+  cepContainer.innerText = 'teste';
+  sectionApi.appendChild(cepContainer);
+});
